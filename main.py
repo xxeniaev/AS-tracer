175aedfb038c445b4192b60eb951a7e4de81983d
xxeniaev <xenia-ev2502@yandex.ru>
2021-04-27 00:59:18 +0500

    detect country and provider

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import re
 import shlex
 import subprocess
 import sys
+import IP2Location
 
 
 def traceroute(address):
@@ -10,7 +11,7 @@ def traceroute(address):
     :param address:
     :return: list of strings with hops info
     """
-    pattern = re.compile(" \d  \* \* \*")
+    pattern = re.compile(".*\d  \* \* \*")
 
     arguments = ["traceroute"] + address
     process = subprocess.Popen(arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -21,7 +22,7 @@ def traceroute(address):
         hop_info_line = element.decode("utf-8")
         if not re.match(pattern, hop_info_line):
             out.append(hop_info_line)
-            # print(hop_info_line)
+            print(hop_info_line)
         else:
             break
 
@@ -60,27 +61,39 @@ def get_as(whois_line):
     pattern = re.compile(".+(?=\\n)")
 
     whois_elements = whois_line.split("|")
-    out = pattern.search(whois_elements[len(whois_elements)-1]).group()
+    out = pattern.search(whois_elements[len(whois_elements) - 1]).group()
 
     return out
 
 
 def make_table(ip_as_dict):
     with open("out.txt", "w") as f:
-        f.write("| Number |         IP         |         AS         |\n")
+        f.write("|  Number  |         IP         |         AS         |          Country          "
+                "|               Provider               |\n")
         i = 0
         for item in ip_as_dict:
             i += 1
-            f.write("| " + str(i) + " " * (7 - len(str(i))) + "| " + str(item[0]) + " " * (19 - len(item[0])) +
-                    "| " + str(item[1]) + " " * (19 - len(item[1])) + "|\n")
+            f.write("| " + str(i) + " " * (9 - len(str(i))) + "| " + str(item[0]) + " " * (19 - len(item[0])) +
+                    "| " + str(item[1]) + " " * (19 - len(item[1])) + "| " + str(item[2]) + " " * (26 - len(item[2])) +
+                    "| " + str(item[3]) + " " * (37 - len(item[3])) + "|\n")
 
 
 if __name__ == '__main__':
+    IP2LocObj = IP2Location.IP2Location()
+    IP2LocObj.open(
+        "IP-COUNTRY-REGION-CITY-LATITUDE-LONGITUDE-ZIPCODE-TIMEZONE-ISP-DOMAIN-NETSPEED-AREACODE-WEATHER-SAMPLE.BIN")
+
     addr = sys.argv[1]
     ip_list = get_ips(traceroute([addr]))
 
+    pattern = re.compile("\d+")
+
     final = []
     for current in ip_list:
-        final.append((current, get_as(whois(current))))
+        country = IP2LocObj.get_country_long(current)
+        provider = '-'
+        if int(pattern.search(current).group(0)) < 100:
+            provider = IP2LocObj.get_isp(current)
+        final.append((current, get_as(whois(current)), country, provider))
 
     make_table(final)
